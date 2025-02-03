e548aa1cdfaf86edb4f7b06f7c3ae76fe0644105
hoang-atr <55073871+hoang-atr@users.noreply.github.com>
2025-02-03 14:59:16 +0900

    Delete RW_model/lik_SM_mean.m

--- RW_model/lik_SM_mean.m
+++
@@ -1,9 +0,0 @@
-function [LH, probSpike] = lik_SM_mean(startValues, spikeCounts)
-
-probSpike = poisspdf(spikeCounts, mean(spikeCounts));
-
-if any(isinf(log(probSpike)))
-    LH = 1e9;
-else
-    LH = -sum(log(probSpike));
-end
