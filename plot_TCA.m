3124b2983d139a85b08465069f14eee00af1acf5
hoang-atr <55073871+hoang-atr@users.noreply.github.com>
2025-02-03 14:44:41 +0900

    Update plot_TCA.m

--- plot_TCA.m
+++ plot_TCA.m
@@ -1,21 +1,10 @@
 clear all
 close all
 
-addpath('tensor_toolbox-v3.2.1/')
-
-hatime_folder = 'ml2ha_data/';
-raw_folder = 'raw_data/';
-files = dir([raw_folder 'Ald*.mat']);
-Ndata = numel(files);
-
 load tensor_data_3trials.mat
 
-%load nn_tensor_3trials_select_idx_top300.mat
-load nn_tensor_3trials_select_idx_top20p.mat
+load nn_tensor_3trials_select_idx_top300.mat
 Ncls = 4;
-
-% load select_idx_zone_3trials_top100.mat
-% Ncls = 2;
 
 Nperf = 3;
 colors = [[0 0 1]; [0 0.8 0]; [1 0 0]];
@@ -23,9 +12,7 @@ colors = [[0 0 1]; [0 0.8 0]; [1 0 0]];
 for c = 1:Ncls
     tt = (-9:40)*0.05;
     
-    for s = 1:Nperf
-%         ix = select_idx{c,s};
-        
+    for s = 1:Nperf        
         ix_stage = find(stage==s);        
         ix = intersect(select_idx{c}, ix_stage);        
         
